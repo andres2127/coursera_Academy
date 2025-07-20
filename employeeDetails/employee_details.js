fd3d4c7ff7a63be4c67d04e961f8455f77ec59aa
Andres <osquitarsanchez48@gmail.com>
2025-07-20 00:43:25 -0400

    Prueba 2

---
+++ employeeDetails/employee_details.js
@@ -0,0 +1,32 @@
+const employees = [
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
+    { id: 2, name: 'Andres Moreno', age: 19, department: 'IT', salary: 100000 },
+    { id: 3, name: 'Mario Saenz', age: 20, department: 'Marketing', salary: 20000 },
+    { id: 4, name: 'Valen Moreno', age: 27, department: 'HR', salary: 30000 },
+];
+
+ // Función para mostrar todos los empleados
+ function displayEmployees(){
+ const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+ document.getElementById('employeesDetails').innerHTML = totalEmployees;
+ }
+function calculateTotalSalaries(){
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: $${totalSalaries}`);
+}
+
+function displayHREmployees() {
+    const hrEmployees = employees.filter(employee => employee.department === 'HR');
+     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+     document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}
+
+function findEmployeeById(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no se ha encontrado ningún empleado con este ID';
+     }
+ }
